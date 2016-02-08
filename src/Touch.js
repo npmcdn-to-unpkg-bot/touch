9dd71e7b89e329da831a946d1b52540fb02b0d3b
rascada <carftjr@gmail.com>
2016-02-08 15:04:13 +0100

    reactive touch
    without hammer

--- src/Touch.js
+++ src/Touch.js
@@ -1,34 +1,45 @@
 'use strict';
 
-import Hammer from 'hammerjs';
+import { Observable } from 'rx';
+const fromEvent = Observable.fromEvent;
 import setTransform from '@sled/set-transform';
 
 class Touch {
-  init(core) {
-    this.hammer = new Hammer(core.domModules.slides);
-    this.slides = core.modules.slides;
-    this.autoSlider = core.modules.autoSlider;
-
-    this.hammer.on('swipe', this.swipe.bind(this));
-    this.hammer.on('pan', this.pan.bind(this));
-    this.hammer.on('panend', this.end.bind(this));
-    this.hammer.on('panstart', this.start.bind(this));
+  constructor() {
+    this.startPosition = null;
   }
 
-  swipe(e) {
-    setTimeout(_ =>
-      this.slides.change(e.direction == 2 ? 1 : -1), 50);
+  init(core) {
+    this.slides = core.modules.slides;
+
+    fromEvent(core.$, 'mousedown')
+      .pluck('clientX')
+      .subscribe(::this.start);
+
+    fromEvent(core.$, 'mouseup')
+      .merge(fromEvent(core.$, 'mouseleave'))
+      .map(::this.delta)
+      .subscribe(::this.stop);
+
+    fromEvent(core.$, 'touchmove')
+      .merge(fromEvent(core.$, 'mousemove'))
+      .filter(_ => this.startPosition !== null)
+      .map(::this.delta)
+      .subscribe(::this.move);
+  }
+
+  delta(e) {
+    return e.clientX - this.startPosition;
   }
 
   nearSlides() {
-    let $slide = this.slides.$.children[this.slides.slide];
+    const $slide = this.slides.$.children[this.slides.slide];
 
     return [$slide.previousElementSibling, $slide, $slide.nextElementSibling];
   }
 
-  pan(e) {
-    let $slides = this.nearSlides();
-    let delta = e.deltaX;
+  move(delta) {
+    const $slides = this.nearSlides();
 
     $slides[1].style.transform = `translateX(${delta}px)`;
 
@@ -36,18 +47,16 @@ class Touch {
     setTransform($slides[2], `translateX(${ $slides[1].offsetWidth + delta }px)`);
   }
 
-  start() {
-    let $slides = this.nearSlides();
+  start(delta) {
+    this.nearSlides()
+      .forEach($slide =>
+        $slide ? $slide.style.transition = '0s' : null);
 
-    $slides.forEach($slide =>
-      $slide ? $slide.style.transition = '0s' : null);
-
-    if (this.autoSlider)
-      this.autoSlider.stop();
+    this.startPosition = delta;
   }
 
-  end(e) {
-    let $slides = this.nearSlides();
+  stop(delta) {
+    const $slides = this.nearSlides();
 
     $slides[1].style.transform = `translateX(0px)`;
 
@@ -57,11 +66,10 @@ class Touch {
     $slides.forEach($slide =>
       $slide ? $slide.style.transition = '1s' : null);
 
-    if (Math.abs(e.deltaX) > ($slides[1].offsetWidth / 2))
-      this.slides.change(e.deltaX > 0 ? -1 : 1);
+    if (Math.abs(delta) > ($slides[1].offsetWidth / 2))
+      this.slides.move(delta > 0 ? -1 : 1);
 
-    if (this.autoSlider)
-      this.autoSlider.start();
+    this.startPosition = null;
   }
 };
 
